789a416116bcc74b4d9d871c8c959c51139f003b
mtakahashi1011 <133080291+mtakahashi1011@users.noreply.github.com>
2023-08-02 00:36:04 -0700

    implement sign-up and sign-in functionality

--- routes/signup.js
+++ routes/signup.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('../db/knex');
 
 router.get('/', function (req, res, next) {
     res.render('signup', {
@@ -9,10 +10,51 @@ router.get('/', function (req, res, next) {
 
 router.post('/', function (req, res, next) {
     console.log(req);
-    console.log(req.body);
+    //console.log(req.body);
     const username = req.body.username;
     const password = req.body.password;
     const repassword = req.body.repassword;
+
+    knex("users")
+        .where({name: username})
+        .select("*")
+        .then(function(result) {
+            if (result.length !== 0) {
+                console.log("This username is already used!");
+                res.render("signup", {
+                    title: "Sign up",
+                    errorMessage: ["This username is already used"]
+                });
+            } else if (password == repassword) {
+                knex("users")
+                    .insert({name: username, password: password})
+                    .then(function() {
+                        res.redirect("/");
+                    })
+                    .catch(function(err) {
+                        console.log("An error is occured!")
+                        console.error(err);
+                        res.render("signup", {
+                            title: "Sign up",
+                            errorMessage: [err.sqlMessage]
+                        });
+                    });
+            } else {
+                console.log("Passwords do not match!");
+                res.render("signup", {
+                    title: "Sign up",
+                    errorMessage: ["Passwords do not match"]
+                });
+            }
+        })
+        .catch(function(err) {
+            console.log("An error is occured!!")
+            console.error(err);
+            res.render("signup", {
+                title: "Sign up",
+                errorMessage: [err.sqlMessage]
+            });
+        });
 });
 
 module.exports = router;
